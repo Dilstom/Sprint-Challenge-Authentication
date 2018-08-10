2ea3c02da98cf9c03d44d483bd4e2500b6cdfb08
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-10 10:10:15 -0700

    Signup page is done

--- server/server.js
+++ server/server.js
@@ -4,17 +4,13 @@ const cors = require('cors');
 const configureRoutes = require('./config/routes');
 
 const server = express();
-const corsOptions = {
-  // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
-  // ensure that your client's URL/Port can achieve a Handshake
-  // then pass this object to the cors() function
-};
+const corsOptions = {};
 
 server.use(express.json());
-server.use(cors());
+server.use(cors({ origin: "http://localhost:3001", credentials: true }));
 
 configureRoutes(server);
 
 module.exports = {
-  server,
+    server,
 };
--- users/src/components/Signup.js
+++ users/src/components/Signup.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Signup extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            username: '',
+            password: ''
+        }
     }
+
+    handleOnchange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .post(`http://localhost:5000/api/register`, this.state)
+            .then(res => {
+                localStorage.setItem('token', res.data);
+                const token = res.data;
+                localStorage.setItem('token', token);
+                this.props.history.push('/jokes');
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
-        return (hi);
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div><input type="text" name='username' value={this.state.username} onChange={this.handleOnchange} /></div>
+                    <div><input type="password" name='password' value={this.state.password} onChange={this.handleOnchange} /></div>
+                    <div>
+                        <button type="submit">
+                            SignIn
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
