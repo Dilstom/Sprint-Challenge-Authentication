64c65ea4bd9a8eab464d4950cff285699ca81c7e
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-10 09:53:11 -0700

    added react-app and created three components

---
+++ users/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, withRouter, Link } from 'react-router-dom';
+
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Jokes from './components/Jokes';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="App-intro">
+            <Link to='/signup'>Sign Up</Link><br/>
+            <Link to='/signin'>Sign In</Link>
+            <Route exact path='/signin' component={ Signin } ></Route>
+            <Route exact path='/jokes' component={ Jokes } ></Route>
+            <Route exact path='/signup' component={ Signup } ></Route>
+        </div>
+      </div>
+    );
+  }
+ 
+}
+
+
+
+export default withRouter(App);
+
---
+++ users/src/components/Signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (signin);
+    }
+}
+
+export default Signin;
